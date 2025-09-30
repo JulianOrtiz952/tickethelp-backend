754fe5df75007db147263a2242da34daa752070f
MajoLvR16 <mariajoselr@ufps.edu.co>
2025-09-29 20:56:06 -0500

    feat(HU03B): rutas /api/users/me y /api/users/me/change-password, también se incluyen variantes por ID para pruebas)

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, AdminViewSet, TechnicianViewSet, ClientViewSet
+from .views import UserViewSet, AdminViewSet, TechnicianViewSet, ClientViewSet, UserUpdateView, ChangePasswordView, ChangePasswordByIdView, UserUpdateByIdView
 from django.urls import path, include
 
 router = DefaultRouter()
@@ -10,4 +10,13 @@ router.register('/clients', ClientViewSet, basename='client')
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    #HU03B - actualizar perfil de usuario
+    path('me/', UserUpdateView.as_view(), name='users-me'), # Endpoint para que el usuario autenticado actualice su perfil
+    path('me/change-password/', ChangePasswordView.as_view(), name='users-change-password'),
+     
+     #Endpoint para las pruebas
+    path('me/<int:pk>/', UserUpdateByIdView.as_view(), name='users-me-by-id'),
+    path('me/change-password/<int:pk>/', ChangePasswordByIdView.as_view(), name='users-change-password-by-id'),
+
 ]

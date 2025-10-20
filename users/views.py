645f1069327fc04651a9f43630c97462d44daae9
LIsaCorrea98 <pruebaslaura19@gmail.com>
2025-10-20 18:33:00 -0500

    feat(HU14A): Agregar campo must_change_password y serializer de autenticación

--- users/views.py
+++ users/views.py
@@ -10,6 +10,11 @@ from tickets.permissions import IsAdminOrTechnicianOrClient
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
 import re
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+import re
 
 from .serializers import (
     UserReadSerializer,
